95e40944334baaa908b10b22429b23091fd91779
coffee-data <jesse.ryan.hartman@gmail.com>
2020-04-28 16:52:00 -0500

    twitter scrape

---
+++ Missions_to_Mars/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import pymongo
+
+# Create an instance for Flask app.
+app = Flask(__name__)
+
+# Create connection variable
+conn = 'mongodb://localhost:27017'
+
+# Pass connection to the pymongo instance.
+client = pymongo.MongoClient(conn)
+
+# Connect to a database. Will create one if not already available.
+db = client.
